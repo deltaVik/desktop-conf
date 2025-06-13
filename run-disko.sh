0b91b691805e42add46f63874917684cd25fa41e
deltaVik <vadimsad2010@yandex.ru>
2025-06-14 02:24:38 +0300

    Fixed typo

--- run-disko.sh
+++ run-disko.sh
@@ -1,7 +1,7 @@
 echo "WARNING: This action will destroy all data on your disks! Please check disko-config.nix before use."
 read -p "This action is irreversible. Are you sure you want to start partitioning the disk? (yes/no): " user_answer
 if [[ "$user_answer" == "yes" ]]; then
-    sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disk-config.nix
+    sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disko-config.nix
 else
     echo "Partitioning aborted."
 fi
